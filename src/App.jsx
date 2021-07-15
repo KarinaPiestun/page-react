bb95c2c8782ee57faba67ea76000ea9458969e60
KarinaPiestun <kpiestun@icloud.com>
2021-07-15 15:33:48 -0300

    header

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+
+import { Header } from './components/Header'
+import {Avatar} from './components/Avatar'
+import {Information} from './components/Information'
+function App() {
+  
+  const [dados, setDados] = useState({})
+  useEffect(() => {
+   
+    fetch('https://api.github.com/users/KarinaPiestun')
+      .then(response => response.json())
+      .then(data => setDados(data))
+  }, [])
+
+  return (
+    <>
+   
+    <Header name={dados.name} location={dados.location} />
+    <div className="main" >
+      <Information name='followers' num={dados.followers}/>
+      <Avatar url={dados.avatar_url} />
+        <Information name='following' num={dados.following} />
+    </div>
+    </>
+  );
+}
+export default App;
+
+
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+
+
+import'./styles.css'
+
+export function Header({name,location}) {
+    return(
+    <header className='header'> 
+      <div className='box-borde'> 
+        <div className='box-name'>
+          <span className='title'>
+           {name}
+          </span>
+          <span className='sub-title'>
+            {location} 
+          </span>
+        </div>
+      </div>
+    </header>
+  )
+}
